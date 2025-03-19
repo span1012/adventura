81a544c51f5c1775b4b4f5bddfc193c0d6e26c09
Karen Sabile <karensabile224@gmail.com>
2025-03-19 16:19:50 -0400

    Update app.py to find similar parks according to user query

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
+import re
 
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
@@ -17,26 +18,71 @@ json_file_path = os.path.join(current_directory, 'yelp.json')
 
 with open(json_file_path, 'r') as file:
     data = json.load(file)
-    park_names = []
-    park_reviews = []
+    park_dict = {}
     for entry in data:
-        reviews = []
-        park_names.append(entry['name'])
-        for review in entry['reviews']:
-            reviews.append(review)
-    parks_df = pd.DataFrame({'Park Name' : park_names, 'Reviews' : reviews})
+        park_dict[entry['name']] = entry['reviews']
 
 app = Flask(__name__)
 CORS(app)
 
+def tokenize(text):
+    """
+    Function to tokenize the given input string.
+    """
+    return re.findall(r'[a-z]+', text.lower())
+
+def aggregate_reviews(park_dict) -> dict[str, dict[str, int]]:
+    """
+    Function to create aggregated term frequency dictionaries for the reviews
+    associated with each distinct amusement park in `reviews_df`.
+    """
+    park_token_dict = {}
+    for park, reviews in park_dict.items():
+        token_dict = {}
+        for review in reviews:
+            tokens = tokenize(review)
+            for token in tokens:
+                if token_dict.get(token) is None:
+                    token_dict[token] = 1
+                else:
+                    token_dict[token] += 1
+        park_token_dict[park] = token_dict
+    return park_token_dict
+
+def find_similar_parks(query_tokens, park_token_dict) -> dict[str, int]:
+    """
+    Function to create and return a dictionary that maps amusement park names to 
+    the cosine similarity scores between their associated tokenized reviews and
+    the input tokenized query. Returns the dictionary sorted in descending
+    cosine similarity score order.
+    """
+    scores = {}
+    n_query_tokens = len(query_tokens)
+    for park, park_tokens in park_token_dict:
+        dot_product = 0
+        common_tokens = 0     # variable to store number of tokens in common
+                              # between the query and the reviews for this park  
+        for token in query_tokens:
+            if park_tokens.get(token) is not None:
+                dot_product += park_tokens.get(token) * query_tokens.get(token)
+                common_tokens += 1
+        total_tokens = n_query_tokens + len(park_tokens) - common_tokens
+        scores[park] = dot_product / total_tokens
+    scores = {park : score for park, score in sorted(scores.items, lambda x:x[1], 
+                                                     reverse=True)}
+    return scores
+
 # Sample search using json with pandas
 def json_search(query):
-    matches = []
-    merged_df = pd.merge(parks_df, reviews_df, left_on='id', right_on='id', how='inner')
-    matches = merged_df[merged_df['name'].str.lower().str.contains(query.lower())]
-    matches_filtered = matches[['name', 'state']]
-    matches_filtered_json = matches_filtered.to_json(orient='records')
-    return matches_filtered_json
+    query_tokens = {}
+    for token in tokenize(query):
+        if query_tokens.get(token) is None:
+            query_tokens[token] = 1
+        else:
+            query_tokens[token] += 1
+    park_token_dict = aggregate_reviews(park_dict)
+    similar_parks = find_similar_parks(query_tokens, park_token_dict)
+    return similar_parks.to_json(orient='records')
 
 @app.route("/")
 def home():
