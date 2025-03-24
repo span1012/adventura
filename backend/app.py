b10d246347596c0079a4de2e4cc2af1adfb4f43a
Sophia Pan <sp2225@cornell.edu>
2025-03-23 23:01:47 -0400

    applied good for kids and location filters

--- backend/app.py
+++ backend/app.py
@@ -20,8 +20,16 @@ with open(json_file_path, 'r') as file:
     data = json.load(file)
     park_dict = {}
     for entry in data:
-        park_dict[entry['name']] = entry['reviews']
-
+        park_dict[entry['business_id']] = {
+            'name': entry['name'],
+            'state': entry['state'],
+            'reviews': entry['reviews']
+        }
+        # if good for kids is in attributes, add it to the dictionary
+        if entry['attributes'] and 'GoodForKids' in entry['attributes']:
+            park_dict[entry['business_id']]['good_for_kids'] = entry['attributes']['GoodForKids']
+        else:
+            park_dict[entry['business_id']]['good_for_kids'] = "False"
 app = Flask(__name__)
 CORS(app)
 
@@ -31,15 +39,15 @@ def tokenize(text):
     """
     return re.findall(r'[a-z]+', text.lower())
 
-def aggregate_reviews(park_dict) -> dict[str, dict[str, int]]:
+def aggregate_reviews(parks) -> dict[str, dict[str, int]]:
     """
     Function to create aggregated term frequency dictionaries for the reviews
     associated with each distinct amusement park in `reviews_df`.
     """
     park_token_dict = {}
-    for park, reviews in park_dict.items():
+    for park, attributes in parks.items():
         token_dict = {}
-        for review in reviews:
+        for review in attributes['reviews']:
             tokens = tokenize(review['text'])
             for token in tokens:
                 if token_dict.get(token) is None:
@@ -49,17 +57,17 @@ def aggregate_reviews(park_dict) -> dict[str, dict[str, int]]:
         park_token_dict[park] = token_dict
     return park_token_dict
 
-def calculate_average_ratings(park_dict) -> dict[str, int]:
+def calculate_average_ratings(parks) -> dict[str, int]:
     """
     Function to calculate each distinct park's average rating, out of five stars,
-    across all of its associated reviews. Returns a dictionary mapping park 
-    names to their average ratings.
+    across all of its associated reviews. Returns a dictionary mapping business 
+    ids to their average ratings.
     """
     rating_dict = {}
-    for park, reviews in park_dict.items():
+    for park, attributes in parks.items():
         rating_sum = 0
         review_count = 0
-        for review in reviews:
+        for review in attributes['reviews']:
             rating_sum += review['stars']
             review_count += 1
         rating_dict[park] = rating_sum / review_count
@@ -67,7 +75,7 @@ def calculate_average_ratings(park_dict) -> dict[str, int]:
 
 def find_similar_parks(query_tokens, park_token_dict) -> dict[str, int]:
     """
-    Function to create and return a dictionary that maps amusement park names to 
+    Function to create and return a dictionary that maps business ids to 
     the cosine similarity scores between their associated tokenized reviews and
     the input tokenized query.
     """
@@ -85,22 +93,46 @@ def find_similar_parks(query_tokens, park_token_dict) -> dict[str, int]:
         scores[park] = (dot_product / total_tokens) * 100
     return scores
 
+def apply_filters(parks, locations=None, good_for_kids=None):
+    """
+    Function to apply location and good for kids filters to the park dictionary.
+    """
+    # filter by location
+    if (parks.items() is None):
+        print("No parks found")
+        return {}
+    if locations is not None and len(locations) > 0:
+        parks = {k: v for (k, v) in parks.items() if v['state'] in locations}
+    # filter by good for kids
+    if good_for_kids == "yes":
+        parks = {k: v for (k, v) in parks.items() if v['good_for_kids'] == "True"}
+    return parks
+
 # Sample search using json with pandas
-def json_search(query):
+def json_search(query, locations=None, good_for_kids=None):
     query_tokens = {}
     for token in tokenize(query):
         if query_tokens.get(token) is None:
             query_tokens[token] = 1
         else:
             query_tokens[token] += 1
-    park_token_dict = aggregate_reviews(park_dict)
+    park_dict_filtered = apply_filters(park_dict, locations, good_for_kids)
+    park_token_dict = aggregate_reviews(park_dict_filtered)
     similarity_scores = find_similar_parks(query_tokens, park_token_dict)
-    average_park_ratings = calculate_average_ratings(park_dict)
-    park_df = pd.DataFrame({"name" : similarity_scores.keys(),
-                            "rating" : average_park_ratings.values(),
-                            "score" : similarity_scores.values()})
+    average_park_ratings = calculate_average_ratings(park_dict_filtered)
+
+    # create a dataframe to store the parks and location and rating information
+    park_df = pd.DataFrame(columns=['name', 'location', 'score', 'rating'])
+    for park, score in similarity_scores.items():
+        new_row = pd.DataFrame({'name': park_dict_filtered[park]['name'],
+                                'location': park_dict_filtered[park]['state'],
+                                'score': score,
+                                'rating': average_park_ratings[park]}, index=[0])
+        park_df = pd.concat([park_df, new_row])
+    # sort the dataframe by score in descending order
     park_df = park_df.sort_values(by='score', ascending=False)
-    return park_df.to_json(orient='records')
+    # return the top 10 parks
+    return park_df.head(10).to_json(orient='records')
 
 @app.route("/")
 def home():
@@ -109,7 +141,14 @@ def home():
 @app.route("/parks")
 def episodes_search():
     text = request.args.get("title")
-    return json_search(text)
+    # apply location filter
+    locations = request.args.get("locations")
+    if locations:
+        locations = locations.split(",")
+        locations = [location.strip() for location in locations]
+    # apply good for kids filter
+    good_for_kids = request.args.get("good_for_kids")
+    return json_search(text, locations, good_for_kids)
 
 if 'DB_NAME' not in os.environ:
     app.run(debug=True,host="0.0.0.0",port=5000)
